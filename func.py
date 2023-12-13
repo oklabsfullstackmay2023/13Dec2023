f318d78e7b07fe5b3960da6952ac6753b59426e9
oklabsfullstackmay2023 <oklabsfullstackmay2023@gmail.com>
2023-12-13 17:00:47 +0530

    func

--- func.py
+++ func.py
@@ -1,6 +1,9 @@
+#1.function defination
 def addition(formalarg1,formalarg2):
     result = formalarg1 + formalarg2
     print(f"Please addition of {formalarg1} and {formalarg2} is {result}") 
+
+#2.function calling/invoking
 addition(5,5)
 addition(50.5,50.5)
 addition(450,450)

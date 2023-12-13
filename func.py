1d596c91882fa87a6cdf32cf619a47d047e7cdb3
oklabsfullstackmay2023 <oklabsfullstackmay2023@gmail.com>
2023-12-13 16:58:28 +0530

    hghg

---
+++ func.py
@@ -0,0 +1,7 @@
+def addition(formalarg1,formalarg2):
+    result = formalarg1 + formalarg2
+    print(f"Please addition of {formalarg1} and {formalarg2} is {result}") 
+addition(5,5)
+addition(50.5,50.5)
+addition(450,450)
+    
